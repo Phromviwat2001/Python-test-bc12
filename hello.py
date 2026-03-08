cd2dd8f3b4795220a9c19b64a7d21b68558e1496
Pu <purock999@gmail.com>
2026-03-08 03:57:26 +0000

    Add new function

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
 ## my frist script
 print("Hello world")
 print("i' am using github with posit.cloud")
+
+def hello():
+  name = input("What is your name: ")
+  print(f"Hello {name}")
+
+## test function hello
+hello()
